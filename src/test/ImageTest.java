0e3989fcb90c1217c18b009cdb541107cd60bd41
wanghongzhou <1012308609@qq.com>
2020-04-21 10:01:00 +0800

    添加图片 并且使用junit测试在classpath下相对路径进行图片资源的访问

--- src/test/ImageTest.java
+++ src/test/ImageTest.java
@@ -16,8 +16,11 @@ class ImageTest {
 	void test() {
 		//fail("Not yet implemented");
 		try {
-			BufferedImage buffer = ImageIO.read(new File("D:/git/tank/tank/src/images/square1.jpg"));
-			assertNotNull(buffer);
+			BufferedImage image_1 = ImageIO.read(new File("D:/git/tank/tank/src/images/square1.jpg"));
+			assertNotNull(image_1);
+		
+			BufferedImage image_2 = ImageIO.read(this.getClass().getClassLoader().getResourceAsStream("images/square1.jpg"));
+			assertNotNull(image_2);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
